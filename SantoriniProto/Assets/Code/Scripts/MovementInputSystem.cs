e8f4056a7db17d88584dfe4db8c811d6c850678a
ScarFratello <scar.fra.02@outlook.it>
2022-09-23 23:07:11 +0200

    Commit progetto iniziale
    Il primo commit contenente il progetto

---
+++ SantoriniProto/Assets/Code/Scripts/MovementInputSystem.cs
@@ -0,0 +1,268 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class MovementInputSystem : MonoBehaviour
+{
+    #region -Variables-
+    [Header("GameObject")]
+    [SerializeField] private CharacterController controller;
+
+    [Header("InputSystem")]
+    private Controls controls;
+
+    [Header("Movement")]
+    [SerializeField] private float speed;
+    Vector3 moveDir;
+    Vector2 direction;
+    //public float speedAnim = 0f;
+    [Header("FollowPath")]
+    [SerializeField] private Transform[] routes;
+    private int routeToGo;
+    private float tParam;
+    private Vector3 objectPosition;
+    [SerializeField] private float speedModifier = .1f;
+    private float normalHit;
+    private bool canCheckNormal;
+
+    [Header("Rotation")]
+    [SerializeField] [Range(0, 1f)] private float turnSmoothTime = .1f;
+    private bool canRotate;
+
+    [Header("Jump")]
+    [SerializeField] private float jumpForce = 3f, highJumpForce = 1f;
+    private float gravity = 3f;
+    private float vSpeed = 0f; // current vertical velocity
+    private bool isGroundedAfterJump = false;
+    #endregion
+
+    private void Awake()
+    {
+        routeToGo = 0;
+        //il player comincia a muoversi dal secondo Control Point
+        tParam = 1f;
+
+        controls = new Controls();
+        controls.Gameplay.Enable();
+        controls.Gameplay.Jump.performed += Jump;
+        controls.Gameplay.HighJump.performed += HighJump;
+    }
+
+    public void Jump(InputAction.CallbackContext context)
+    {
+        //context.performed è quando premiamo il tasto
+        if (context.performed && controller.isGrounded)
+        {
+            vSpeed = jumpForce;
+        }
+    }
+
+    public void HighJump(InputAction.CallbackContext context)
+    {
+        //context.performed è quando premiamo il tasto
+        if (context.performed && controller.isGrounded)
+        {
+            vSpeed = jumpForce + highJumpForce;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        direction = new Vector2(controls.Gameplay.Move.ReadValue<Vector2>().x, 0f);
+
+        Vector3 p0 = routes[routeToGo].GetChild(0).position;
+        Vector3 p1 = routes[routeToGo].GetChild(1).position;
+        Vector3 p2 = routes[routeToGo].GetChild(2).position;
+        Vector3 p3 = routes[routeToGo].GetChild(3).position;
+
+        #region -SetAnimation-
+        if (!controller.isGrounded)
+        {
+            //Animate("jumpNo1");
+            //se non tocca il suolo è pronto per far partire l'animazione non appena lo toccherà
+            isGroundedAfterJump = true;
+        }
+        else
+        {
+            //qui parte l'animazione se prima non stava toccando il suolo
+            if (isGroundedAfterJump)
+            {
+                //Animate("postJumpNo1");
+                isGroundedAfterJump = false;
+            }
+            else
+            {
+                if (direction.magnitude >= .5f)
+                {
+                    //speedAnim = direction.magnitude;
+                    //anim.SetFloat("speed", speedAnim);
+                    //Animate("runNo1");
+                }
+
+                else if (direction.magnitude > .15f)
+                {
+                    //speedAnim = direction.magnitude;
+                    //anim.SetFloat("speed", speedAnim);
+                    //Animate("walkNo1");
+                }
+
+                else
+                {
+                    //Animate("idleNo1");
+                }
+            }
+
+        }
+        #endregion
+
+        if (direction.magnitude > .15f)
+        {
+            #region -Rotation-
+            //Atan2 ci d? l'angolazione in radianti dall'asse x
+            //prende l'angolazione del nostro movimento e la somma a 180
+            float targetAngle = Mathf.Atan2(direction.x, direction.y) * Mathf.Rad2Deg + 180;
+            #endregion
+
+            moveDir = Quaternion.Euler(0f, targetAngle, 0f) * new Vector3(0f, 0f, direction.magnitude);
+
+            //tParam sarebbe la posizione nella route che va da 0 a 1 (0 è l'inizio e 1 la fine)
+            if (tParam <= 1 && tParam >= 0)
+            {
+                if ((controller.collisionFlags & CollisionFlags.Sides) == 0)
+                {
+                    //non stai urtando
+                    //anche se andiamo avanti quando stiamo sotto si modifica perché moveDir.x cambia sempre
+                    tParam += moveDir.x * speedModifier;
+                    canRotate = true;
+                }
+                else
+                {
+                    //Sta urtando
+                    if (Mathf.Sign(normalHit) >= 0f)
+                    {
+                        if(moveDir.x > 0)
+                        {
+                            tParam += moveDir.x * speedModifier;
+                        }
+                    }
+                    else
+                    {
+                        if (moveDir.x < 0)
+                        {
+                            tParam += moveDir.x * speedModifier;
+                        }
+                    }
+                }
+
+                objectPosition = Mathf.Pow(1 - tParam, 3) * p0 + 3 *
+                    Mathf.Pow(1 - tParam, 2) * tParam * p1 + 3 * (1 - tParam) *
+                    Mathf.Pow(tParam, 2) * p2 +
+                    Mathf.Pow(tParam, 3) * p3;
+                //Debug.Log("Questo è objectPosition: " + objectPosition);
+
+                //Debug.Log("Questo è tParam: " + tParam);
+            }
+            else
+            {
+                //se supera la fine di quella determinata route in cui ci troviamo
+                if (tParam > 1)
+                {
+                    tParam = 0f;
+
+                    routeToGo += 1;
+
+                    if (routeToGo > routes.Length - 1)
+                    {
+                        //vediamo che fare quando arriva alla fine
+                        Debug.Log("Sei alla fine della route");
+                    }
+                }
+
+                //se torna indietro la route si ricollega alla route precedente
+                else if (tParam < 0)
+                {
+                    tParam = 1f;
+
+                    routeToGo -= 1;
+
+                    if (routeToGo > routes.Length - routes.Length)
+                    {
+                        //vediamo che fare quando arriva all'inizio
+                        Debug.Log("Sei all'inizio della route");
+                    }
+                }
+            }
+        }
+
+        if (vSpeed > -5)
+        {
+            vSpeed -= gravity * Time.deltaTime;
+        }
+        
+        Debug.Log("vSpeed è: " + vSpeed);
+
+        MoveToPoint(objectPosition, vSpeed);
+    }
+
+    public void MoveToPoint(Vector3 targetPosition, float vSpeed)
+    {
+
+        Vector3 movDiff = targetPosition - transform.position;
+        movDiff.y = vSpeed;
+
+        Vector3 movRot = movDiff;
+
+        movRot.y = 0;
+
+        if (canRotate)
+        {
+            transform.rotation = Quaternion.LookRotation(movRot);
+        }
+        
+        controller.Move(movDiff * speed * Time.deltaTime);
+    }
+
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        if ((controller.collisionFlags & CollisionFlags.Sides) != 0 && canCheckNormal)
+        {
+            normalHit = hit.normal.x + hit.normal.z;
+            canCheckNormal = false;
+            canRotate = false;
+        }
+        else
+        {
+            canCheckNormal = true;
+        }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        // Draw a yellow sphere at the objectPosition position
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawSphere(objectPosition, 1);
+    }
+
+    #region -Animation-
+    /*private void Animate(string animName)
+    {
+        DisableOtherAnim(anim, animName);
+
+        anim.SetBool(animName, true);
+    }
+
+    private void DisableOtherAnim(Animator anim, string animName)
+    {
+        foreach (AnimatorControllerParameter parameter in anim.parameters)
+        {
+            if (parameter.name != "speed")
+            {
+                if (parameter.name != animName)
+                {
+                    anim.SetBool(parameter.name, false);
+                }
+            }
+
+        }
+    }*/
+    #endregion
+}
---
+++ SantoriniProto/Assets/Code/Scripts/MovementPlayer.cs
@@ -0,0 +1,140 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class MovementPlayer : MonoBehaviour
+{
+    private Controls controls;
+    private float h = 0f;
+    private float v = 0f;
+
+    [Header("Movement Variable")]
+    [SerializeField] private float movementSpeed;
+    [Header("Jump Forces Reference")]
+    [SerializeField] private float jumpForce = 3f, highJumpForce = 1f;
+    [Header("Can Movement Container")]
+    [SerializeField] private bool canMove;
+    [Header("Rigidbody Container")]
+    [SerializeField] private Rigidbody rb;
+
+    public enum PlayerState { WALKING, FALLING }
+    [Header("Player State Reference")]
+    public PlayerState state = PlayerState.WALKING;
+
+    private bool jumpDown = false, highJumpDown = false;
+    private float jumpForceBackup;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        canMove = true;
+        jumpForceBackup = jumpForce;
+        controls = new Controls();
+        controls.Gameplay.Enable();
+        controls.Gameplay.Jump.performed += JumpPerformed;
+        controls.Gameplay.HighJump.performed += HighJump;
+    }
+
+    public void JumpPerformed(InputAction.CallbackContext context)
+    {
+        //.performed è quando premiamo il tasto
+        if (!jumpDown)
+        {
+            jumpDown = true;
+        }
+    }
+
+    public void HighJump(InputAction.CallbackContext context)
+    {
+        if (!highJumpDown)
+        {
+            highJumpDown = true;
+        }
+    }
+
+    private void Update()
+    {
+        if (canMove)
+        {
+            // Input happens per-frame not in the Physics Loop
+            h = controls.Gameplay.Move.ReadValue<Vector2>().x;
+            v = controls.Gameplay.Move.ReadValue<Vector2>().y;
+        }
+        else
+        {
+            h = 0;
+            v = 0;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 input = SquareToCircle(new Vector2(h, v));
+
+        Transform cam = Camera.main.transform;
+        Vector3 moveDirection = Quaternion.FromToRotation(cam.up, Vector3.up)
+            * cam.TransformDirection(new Vector3(input.x, 0f, input.y));
+
+        switch (state)
+        {
+            case PlayerState.WALKING: { HandleWalking(moveDirection); } break;
+            case PlayerState.FALLING: { HandleFalling(moveDirection); } break;
+        }
+
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, Vector3.down, out hit, 1.2f))
+        {
+            state = PlayerState.WALKING;
+        }
+        //passa a falling dopo walking se saltiamo una volta, eliminarlo per fare il doppio salto
+        else if (state == PlayerState.WALKING)
+        {
+            state = PlayerState.FALLING;
+        }
+
+        //Reset input
+        jumpDown = false;
+        highJumpDown = false;
+        jumpForce = jumpForceBackup;
+    }
+
+    public void HandleWalking(Vector3 moveDirection)
+    {
+        if (jumpDown)
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            state = PlayerState.FALLING;
+        }
+        else if (highJumpDown)
+        {
+            rb.AddForce(Vector3.up * (jumpForce + highJumpForce), ForceMode.Impulse);
+            state = PlayerState.FALLING;
+        }
+
+        rb.velocity = new Vector3(moveDirection.x * movementSpeed, rb.velocity.y, moveDirection.z * movementSpeed);
+
+        //fa ruotare il personaggio, e la condizione è la sensibilità
+        if (moveDirection.sqrMagnitude > 0.01f)
+        {
+            transform.forward = Vector3.Lerp(transform.forward,
+                                            moveDirection,
+                                            10f * Time.fixedDeltaTime);
+        }
+    }
+
+    public void HandleFalling(Vector3 moveDirection)
+    {
+        rb.velocity = new Vector3(moveDirection.x * movementSpeed, rb.velocity.y, moveDirection.z * movementSpeed);
+        //fa ruotare il personaggio, e la condizione è la sensibilità
+        if (moveDirection.sqrMagnitude > 0.01f)
+        {
+            transform.forward = Vector3.Lerp(transform.forward,
+                                            moveDirection,
+                                            10f * Time.fixedDeltaTime);
+        }
+    }
+
+    Vector2 SquareToCircle(Vector2 input)
+    {
+        return (input.sqrMagnitude >= 1f) ? input.normalized : input;
+    }
+}
