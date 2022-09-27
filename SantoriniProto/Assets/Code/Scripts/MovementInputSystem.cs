ffdd06a2f8b04e184862820e893ac83a14af7c46
ScarFratello <scar.fra.02@outlook.it>
2022-09-27 17:47:30 +0200

    Commit metà tracciato

--- SantoriniProto/Assets/Code/Scripts/MovementInputSystem.cs
+++ SantoriniProto/Assets/Code/Scripts/MovementInputSystem.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -27,13 +29,17 @@ public class MovementInputSystem : MonoBehaviour
 
     [Header("Rotation")]
     [SerializeField] [Range(0, 1f)] private float turnSmoothTime = .1f;
-    private bool canRotate;
+    private bool canRotate = true;
 
     [Header("Jump")]
     [SerializeField] private float jumpForce = 3f, highJumpForce = 1f;
     private float gravity = 3f;
     private float vSpeed = 0f; // current vertical velocity
     private bool isGroundedAfterJump = false;
+    [Header("Collision")]
+    private bool isHittedEnemy;
+    [SerializeField]
+    private float enemyPushForce = .3f;
     #endregion
 
     private void Awake()
@@ -135,15 +141,19 @@ public class MovementInputSystem : MonoBehaviour
                     tParam += moveDir.x * speedModifier;
 
                     lastDir = moveDir;
-
-                    canRotate = true;
                 }
                 else
                 {
                     //Sta urtando
                     if (Mathf.Sign(normalHit) < 0f)
                     {
-                        if(lastDir.x != moveDir.x)
+                        if (isHittedEnemy)
+                        {
+                            tParam += (-moveDir.x -lastDir.x) * enemyPushForce;
+                            TParamLoop();
+                            isHittedEnemy = false;
+                        }
+                        else if(lastDir.x != moveDir.x)
                         {
                             tParam += moveDir.x * speedModifier;
                         }
@@ -157,33 +167,7 @@ public class MovementInputSystem : MonoBehaviour
             }
             else
             {
-                //se supera la fine di quella determinata route in cui ci troviamo
-                if (tParam > 1)
-                {
-                    tParam = 0f;
-
-                    routeToGo += 1;
-
-                    if (routeToGo > routes.Length - 1)
-                    {
-                        //vediamo che fare quando arriva alla fine
-                        Debug.Log("Sei alla fine della route");
-                    }
-                }
-
-                //se torna indietro la route si ricollega alla route precedente
-                else if (tParam < 0)
-                {
-                    tParam = 1f;
-
-                    routeToGo -= 1;
-
-                    if (routeToGo > routes.Length - routes.Length)
-                    {
-                        //vediamo che fare quando arriva all'inizio
-                        Debug.Log("Sei all'inizio della route");
-                    }
-                }
+                TParamLoop();
             }
         }
 
@@ -218,20 +202,62 @@ public class MovementInputSystem : MonoBehaviour
         controller.Move(movDiff * speed * Time.deltaTime);
     }
 
+    private void TParamLoop()
+    {
+        //se supera la fine di quella determinata route in cui ci troviamo
+        if (tParam > 1)
+        {
+            tParam = 0f;
+
+            routeToGo += 1;
+
+            if (routeToGo > routes.Length - 1)
+            {
+                //vediamo che fare quando arriva alla fine
+                Debug.Log("Sei alla fine della route");
+            }
+        }
+
+        //se torna indietro la route si ricollega alla route precedente
+        else if (tParam < 0)
+        {
+            tParam = 1f;
+
+            routeToGo -= 1;
+
+            if (routeToGo > routes.Length - routes.Length)
+            {
+                //vediamo che fare quando arriva all'inizio
+                Debug.Log("Sei all'inizio della route");
+            }
+        }
+    }
+
     private void OnControllerColliderHit(ControllerColliderHit hit)
     {
         if ((controller.collisionFlags & CollisionFlags.Sides) != 0 && canCheckNormal)
         {
             //normalHit = hit.normal.x + hit.normal.z;
             normalHit = Vector3.Dot(transform.forward, hit.normal);
+            if (hit.gameObject.layer == LayerMask.NameToLayer("Enemy"))
+            {
+                isHittedEnemy = true;
+                StartCoroutine(CanRotate());
+            }
             canCheckNormal = false;
-            canRotate = false;
             //Debug.Log("forward " + transform.forward + "normal " + hit.normal + "normal hit" + normalHit);
         }
         else
         {
             canCheckNormal = true;
         }
+    }
+
+    IEnumerator CanRotate()
+    {
+        canRotate = false;
+        yield return new WaitForSeconds(.3f);
+        canRotate = true;
     }
 
     void OnDrawGizmosSelected()
