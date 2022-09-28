a53d349d39ebf2beb971e83947dc10420851a860
Martin <gibilterramartin@gmail.com>
2022-09-28 12:10:11 +0200

    Update StepOnEnemyJump.cs

--- SantoriniProto/Assets/Code/Scripts/StepOnEnemyJump.cs
+++ SantoriniProto/Assets/Code/Scripts/StepOnEnemyJump.cs
@@ -8,12 +8,13 @@ public class StepOnEnemyJump : MonoBehaviour
     [SerializeField] private float stepOnJumpForce = 1f;
     [SerializeField]
     private LayerMask enemyMask;
+    private RaycastHit enemyHit;
 
     private void Update()
     {
         Vector3 down = transform.TransformDirection(Vector3.down);
 
-        if (Physics.Raycast(transform.position, down, .3f, enemyMask))
+        if (Physics.Raycast(transform.position, down, out enemyHit, .3f, enemyMask))
         {
             mis.vSpeed = stepOnJumpForce;
         }
